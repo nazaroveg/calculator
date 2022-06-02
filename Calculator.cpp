dca7a11470338e912414d9a2a47b65a21fcde3f6
nazaroveg <nazaroveg@yandex.ru>
2022-06-02 14:23:09 +0300

    xz

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,16 +7,12 @@ class calculator
 {
 public:
 	
-	double num_1;
-	double num_2;
-	double result;
-	std::string sign;
 
-	bool set_num1(double &num_1)
+	bool set_num1(double num_1)
 	{
 		return (num_1 == 0) ? true : false;
 	}
-	bool set_num2(double &num2)
+	bool set_num2(double num_2)
 	{
 		return (num_2 == 0) ? true : false;
 	}
@@ -62,21 +58,44 @@ public:
 	{
 		std::cout << "num_2 " << sign << " num_1 = " << result << std::endl;
 	}
-	void read(double& num_1, double& num_2)
-	{
-		std::cout << "Введите num1: \n";
-		std::cin >> num_1;
-		std::cout << "Введите num2: \n";
-		std::cin >> num_2;
-	}
+	
+	void doprint(double num_1,double num_2)
+	{   
+		std::string sign;
+		add(num_1, num_2); //+
+		sign = "+";
+		print(sign, result);
+
+		subtract_2_1(num_1, num_2);  //a-b
+		sign = "-";
+		print(sign, result);
+
+		subtract_1_2(num_1, num_2);  //b-a
+		sign = "-";
+		print_2(sign, result);
+
+		multiply(num_1, num_2);   // *
+		sign = "*";
+		print(sign, result);
+
+		divide_1_2(num_1, num_2);  // a/b
+		sign = "/";
+		print(sign, result);
+
+		divide_2_1(num_1, num_2);   //b/ a
+		sign = "/";
+		print_2(sign, result);
 		
+		
+	}
 
 	
 	
 
 private:
-
-
+	double num_1;
+	double num_2;
+	double result;
 };
 
 
@@ -86,50 +105,31 @@ private:
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	calculator calc{}, calc_2{};
-
+	calculator calc{};
+	double nam_1, nam_2 = 0;
 	for (;;)
 	{
-		calc.read(calc.num_1, calc_2.num_2);  //read keyboard
-		calc.set_num1(calc.num_1);
-		if (calc.num_1 == false)
+		
+		
+		
+			std::cout << "Введите num1: \n";
+			std::cin >> nam_1;
+			std::cout << "Введите num2: \n";
+			std::cin >> nam_2;
+		
+		calc.set_num1(nam_1);
+		if (nam_1 == false)
 		{
 			std::cout << "Неверный ввод!\n ";
 			continue;
 		}
-		calc.set_num2(calc.num_2);
-		if (calc.num_2 == false)
+		calc.set_num2(nam_2);
+		if (nam_2 == false)
 		{
 			std::cout << "Неверный ввод!\n ";
 			continue;
 		}
-		
-		
-
-		calc.add(calc.num_1, calc_2.num_2); //+
-		calc.sign = "+";
-		calc.print(calc.sign, calc.result);
-		
-		calc.subtract_2_1(calc.num_1, calc_2.num_2);  //a-b
-		calc.sign = "-";
-		calc.print(calc.sign, calc.result);
-
-		calc.subtract_1_2(calc.num_1, calc_2.num_2);  //b-a
-		calc.sign = "-";
-		calc.print_2(calc.sign, calc.result);
-		
-		calc.multiply(calc.num_1, calc_2.num_2);   // *
-		calc.sign = "*";
-		calc.print(calc.sign, calc.result);
-		
-		calc.divide_1_2(calc.num_1, calc_2.num_2);  // a/b
-		calc.sign = "/";
-		calc.print(calc.sign, calc.result);
-
-		calc.divide_2_1(calc.num_1, calc_2.num_2);   //b/ a
-		calc.sign = "/";
-		calc.print_2(calc.sign, calc.result);
-
-
+		calc.doprint(nam_1, nam_2);
 	}
+	   
 }
